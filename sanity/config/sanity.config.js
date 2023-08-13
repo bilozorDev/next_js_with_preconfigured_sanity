60bb61e496cf6f0c49e07d84ed98776b6ba5aa13
Alex Bilozor <bilozor.dev@gmail.com>
2023-08-12 20:38:34 -0500

    removed projectID from config

--- sanity/config/sanity.config.js
+++ sanity/config/sanity.config.js
@@ -7,7 +7,7 @@ import { myStructure } from "./deskStructure";
 export default defineConfig({
  
   apiVersion: '2023-08-12', // Today's date in YYYY-MM-DD format
-  projectId: '9rqia0s3', // Your project ID from sanity.io
+  projectId: '', // Your project ID from sanity.io
   dataset: "production", // Your dataset name from sanity.io (default 'production')
   basePath: "/admin-panel", // The path you want to access Sanity Studio from
   plugins: [
--- sanity/utils/client.js
+++ sanity/utils/client.js
@@ -4,7 +4,7 @@ const client = createClient({
 
    
     apiVersion: '2023-08-12', // Today's date in YYYY-MM-DD format
-  projectId: '9rqia0s3', // Your project ID from sanity.io
+  projectId: '', // Your project ID from sanity.io
   dataset: "production", // Your dataset name from sanity.io (default 'production')
 })
 
